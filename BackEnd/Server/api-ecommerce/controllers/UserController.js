28dd5e002abb9030a35e74dfaecd81c7175e01e5
FrankAyalaSol <201810595@urp.edu.pe>
2024-06-27 14:39:03 -0500

    Revert "Implementación de un email de confirmación en el Módulo Cliente"
    This reverts commit cd4b400d8cdff623ec5cb05d6353ec5d2f505664.

--- BackEnd/Server/api-ecommerce/controllers/UserController.js
+++ BackEnd/Server/api-ecommerce/controllers/UserController.js
@@ -2,60 +2,19 @@ import bcript from 'bcryptjs'
 import models from '../models';
 import token from '../service/token';
 import resource from '../resource';
-import nodemailer from 'nodemailer';
 
 
 export default{
-    
-    register: async (req, res) => {
+    register: async(req, res)=>{
         try {
-            const existingUser = await models.User.findOne({ email: req.body.email });
-            if (existingUser) {
-                return res.status(400).send({
-                    message: 'El email ya está registrado. Por favor, use otro email.'
-                });
-            }
-
-            req.body.password = await bcript.hash(req.body.password, 10);
-            req.body.confirmationToken = token.generateConfirmationToken();
+            req.body.password = await bcript.hash(req.body.password,10);
             const user = await models.User.create(req.body);
-
-            // Enviar email de confirmación
-            const confirmationUrl = `http://localhost:3000/api/user/confirm/${req.body.confirmationToken}`;
-            
-            const transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'frank3000100@gmail.com',
-                    pass: 'lyth bbbf eqko pqaq'
-                }
-            });
-
-            const mailOptions = {
-                from: 'frank3000100@gmail.com',
-                to: req.body.email,
-                subject: 'Email de confirmación',
-                html: `
-                    <p>Gracias por registrarte en nuestra plataforma.</p>
-                    <p>Por favor, confirma tu email haciendo clic en el siguiente enlace:</p>
-                    <p><a href="${confirmationUrl}">${confirmationUrl}</a></p>
-                    <p>Si no has solicitado este registro, puedes ignorar este mensaje.</p>
-                `
-            };
-
-            transporter.sendMail(mailOptions, (error, info) => {
-                if (error) {
-                    return res.status(500).send({
-                        message: 'Error enviando el email'
-                    });
-                }
-                res.status(200).json(user);
-            });
+            res.status(200).json(user);
         } catch (error) {
             res.status(500).send({
-                message: 'OCURRIÓ UN PROBLEMA'
+                message: "OCURRIÓ UN PROBLEMA"
             });
-            console.log(error);
+            console.log(error)
         }
     },
     register_admin: async(req, res)=>{
@@ -79,40 +38,17 @@ export default{
             console.log(error)
         }
     },
-    confirmEmail: async (req, res) => {
+    login: async(req, res)=>{
         try {
-            const { token } = req.params;
-            const user = await models.User.findOne({ confirmationToken: token });
-            if (!user) {
-                return res.status(400).send({ message: 'Token inválido o expirado' });
-            }
-            user.emailConfirmed = true;
-            user.confirmationToken = null; // Elimina el token después de confirmar
-            await user.save();
-            res.status(200).send({ message: 'Email confirmado exitosamente' });
-        } catch (error) {
-            res.status(500).send({
-                message: 'OCURRIÓ UN PROBLEMA'
-            });
-            console.log(error);
-        }
-    },
-    login: async (req, res) => {
-        try {
-            const user = await models.User.findOne({ email: req.body.email, state: 1 });
-            if (user) {
-                if (!user.emailConfirmed) {
-                    return res.status(400).send({
-                        message: 'Debes confirmar tu email antes de iniciar sesión'
-                    });
-                }
+            const user = await models.User.findOne({email: req.body.email, state:1})
+            if(user){
                 let compare = await bcript.compare(req.body.password, user.password);
-                if (compare) {
-                    let tokenT = await token.encode(user._id, user.rol, user.email);
+                if(compare){
+                    let tokenT = await token.encode(user._id,user.rol,user.email)
 
                     const USER_FRONTEND = {
-                        token: tokenT,
-                        user: {
+                        token:tokenT,
+                        user:{
                             name: user.name,
                             email: user.email,
                             surname: user.surname,
@@ -120,23 +56,23 @@ export default{
                         },
                     }
                     res.status(200).json({
-                        USER_FRONTEND: USER_FRONTEND,
-                    });
-                } else {
+                        USER_FRONTEND:USER_FRONTEND,
+                    })
+                }else{
                     res.status(500).send({
-                        message: 'EL USUARIO NO EXISTE'
+                        message: "EL USUARIO NO EXISTE"
                     });
                 }
-            } else {
+            }else{
                 res.status(500).send({
-                    message: 'EL USUARIO NO EXISTE'
+                    message: "EL USUARIO NO EXISTE"
                 });
             }
         } catch (error) {
             res.status(500).send({
-                message: 'OCURRIÓ UN PROBLEMA'
+                message: "OCURRIÓ UN PROBLEMA"
             });
-            console.log(error);
+            console.log(error)
         }
     },
     login_admin: async(req, res)=>{
@@ -206,7 +142,7 @@ export default{
     list: async(req, res)=>{
         try {
             var search = req.query.search;
-            let User = await models.User.find(
+            let Users = await models.User.find(
                 {
                     $or:[
                         {"name":new RegExp(search, "i")},
@@ -216,12 +152,12 @@ export default{
                 }
             ).sort({'createdAt':-1});
 
-            User = User.map((user)=>{
+            Users = Users.map((user)=>{
                 return resource.User.user_list(user);
             })
 
             res.status(200).json({
-                user: User
+                users: Users
             });
         } catch (error) {
             res.status(500).send({
