c527f7f808a9b067684455376b6fc262d81038c0
FrankAyalaSol <201810595@urp.edu.pe>
2024-08-03 09:52:38 -0500

    Ajuste de Edición de Variedad - ADMIN METRONIC

--- admin_metronic/src/app/modules/product/variedades/edit-new-variedad/edit-new-variedad.component.ts
+++ admin_metronic/src/app/modules/product/variedades/edit-new-variedad/edit-new-variedad.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { ProductService } from '../../_services/product.service';
+import { Toaster } from 'ngx-toast-notifications';
+import { NoticyAlertComponent } from 'src/app/componets/notifications/noticy-alert/noticy-alert.component';
 
 @Component({
   selector: 'app-edit-new-variedad',
@@ -14,18 +16,26 @@ export class EditNewVariedadComponent implements OnInit {
 
   isLoading$:any;
   variedad_multiple:any = null;
+  stock:any = 0;
   constructor(
     public modal:NgbActiveModal,
-    public _serviceProduct:ProductService
+    public _serviceProduct:ProductService,
+    public toaster: Toaster,
   ) { }
 
   ngOnInit(): void {
     this.variedad_multiple = this.variedad.valor;
+    this.stock = this.variedad.stock;
   }
   update(){
+    if (this.variedad_multiple == ''){
+      this.toaster.open(NoticyAlertComponent,{text:`danger- 'DEBE DE INGRESAR UNA VARIEDAD'`})
+      return;
+    }
     let data = {
       _id: this.variedad._id,
       valor: this.variedad_multiple,
+      stock: this.stock
     }
     this._serviceProduct.updateVariedad(data).subscribe((resp:any) =>{
       console.log(resp);
